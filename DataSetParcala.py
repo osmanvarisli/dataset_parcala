62d50e424c40454b5a7598e3ab101bd5cc31795e
Osman VARIŞLI <64133166+osmanvarisli@users.noreply.github.com>
2021-12-29 12:12:30 +0300

    Add files via upload

---
+++ DataSetParcala.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 10 09:40:34 2021
+
+@author: Bigislem5
+"""
+import os
+import numpy as np
+import shutil
+
+path = "COVID-19_Radiography_Dataset/"
+
+classesPath = ['COVID', 'Lung_Opacity','Viral Pneumonia', 'NORMAL']
+
+val_ratio = 0.2
+test_ratio = 0.1
+
+for cls in classesPath:
+    os.makedirs('./train/' + cls)
+    os.makedirs('./val/' + cls)
+    os.makedirs('./test/' + cls)
+
+    src = path + cls
+
+    allFileNames = os.listdir(src)
+    np.random.shuffle(allFileNames)
+    train_FileNames, val_FileNames, test_FileNames = np.split(np.array(allFileNames),
+                                                              [int(len(allFileNames) * (1 - (val_ratio + test_ratio))), 
+                                                               int(len(allFileNames) * (1 - test_ratio))])
+
+    train_FileNames = [src + '/' + name for name in train_FileNames.tolist()]
+    val_FileNames = [src + '/' + name for name in val_FileNames.tolist()]
+    test_FileNames = [src + '/' + name for name in test_FileNames.tolist()]
+
+    print('Total ', str(cls), len(allFileNames))
+    print('Training', len(train_FileNames))
+    print('Validation', len(val_FileNames))
+    print('Testing', len(test_FileNames))
+    print("\n")
+
+    for name in train_FileNames:
+        shutil.copy(name, './train/' + cls)
+
+    for name in val_FileNames:
+        shutil.copy(name, './val/' + cls)
+
+    for name in test_FileNames:
+        shutil.copy(name, './test/' + cls)
